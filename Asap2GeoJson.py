339c63e6e3e9b9687dc93769eedb409c7603cf69
Onno <o.j.deboer@amc.uva.nl>
2023-10-16 12:40:51 +0200

    All classes

--- Asap2GeoJson.py
+++ Asap2GeoJson.py
@@ -5,6 +5,23 @@ from geojson import Feature, FeatureCollection, dump
 # path = "C:\\Users\\Onno\\data\\data\\pancreas"
 path = "D:\\qupath\\"
 
+classes = {"roi": [130, 76, 157],
+           "cancer_ducts": [218, 115, 235],
+           "normal_ducts": [192, 91, 8],
+           "islets": [9, 118, 25],
+           "nerves": [116, 96, 249],
+           "lyfocytic_infiltrate": [106, 236, 190],
+           "vessels": [240, 205, 62],
+           "fat":[211, 49, 153],
+           "atrophic_metaplastic_parenchyma": [222, 50, 123],
+           "acinic_tissue": [194, 24, 92],
+           "mucinious_dissection": [113, 29, 84],
+           "uncertain": [87, 230, 174],
+           "duodenum": [120, 150, 241],
+           "ignore": [180, 57, 18],
+           "in_situ_neoplasia": [226, 34, 198]
+            }
+
 for file in os.listdir(path):
     if file.endswith(".xml"):
         tree = ET.parse(os.path.join(path, file))
@@ -12,35 +29,21 @@ for file in os.listdir(path):
 
         features = []
 
-        roi_elements = root.findall('.//Annotation[@PartOfGroup="roi"]')
-        for roi_element in roi_elements:
-            coords = []
-            for coordinate in roi_element.findall('.//Coordinate'):
-                x = float(coordinate.get('X'))  # Convert to float if needed
-                y = float(coordinate.get('Y'))
-                coords.append([x, y])
+        for key, value in classes.items():
 
-            coords.append(coords[0])  # Close the polygon by adding the first point again
-            feature = Feature(geometry={"type": "Polygon", "coordinates": [coords]},
-                              properties={"objectType": "annotation",
-                              "classification": {"name": "roi", "color": [130, 76, 157]}})
-            features.append(feature)
+            roi_elements = root.findall(f'.//Annotation[@PartOfGroup="{key}"]')
+            for roi_element in roi_elements:
+                coords = []
+                for coordinate in roi_element.findall('.//Coordinate'):
+                    x = float(coordinate.get('X'))  # Convert to float if needed
+                    y = float(coordinate.get('Y'))
+                    coords.append([x, y])
 
-        roi_elements = root.findall('.//Annotation[@PartOfGroup="cancer_ducts"]')
-        for roi_element in roi_elements:
-            coords = []
-            for coordinate in roi_element.findall('.//Coordinate'):
-                x = float(coordinate.get('X'))  # Convert to float if needed
-                y = float(coordinate.get('Y'))
-                coords.append([x, y])
-
-            coords.append(coords[0])  # Close the polygon by adding the first point again
-            feature = Feature(geometry={"type": "Polygon", "coordinates": [coords]},
-                              properties={"objectType": "annotation",
-                                          "classification": {"name": "cancer_ducts", "color": [218, 115, 235]}})
-
-            features.append(feature)
-
+                coords.append(coords[0])  # Close the polygon by adding the first point again
+                feature = Feature(geometry={"type": "Polygon", "coordinates": [coords]},
+                                  properties={"objectType": "annotation",
+                                  "classification": {"name": key, "color": value}})
+                features.append(feature)
 
         feature_collection = FeatureCollection(features)
 
